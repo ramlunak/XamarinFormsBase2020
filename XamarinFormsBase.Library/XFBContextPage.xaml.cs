83629ce062c27e50ef435190623dd14d43dfb41a
royber <royber@yardim.com.br>
2020-04-14 06:48:13 -0300

    Add project files.

---
+++ XamarinFormsBase.Library/XFBContextPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsBase.Library
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class XFBContextPage : ContentPage
+    {
+        public XFBContextPage(ContentView contentView)
+        {
+            InitializeComponent();
+        }
+    }
+}
